77251cd7f2e68b8b21d9bce8d17ff9cca6e83ee9
E-Mello <e.mello.dev@gmail.com>
2024-02-16 15:57:02 -0400

    Add explosion animation to canvas

--- Project_06_Triggered_event_based_animations/removeEffect/script.js
+++ Project_06_Triggered_event_based_animations/removeEffect/script.js
@@ -18,27 +18,20 @@ class Explosion {
     this.spriteHeight = 179;
     this.width = this.spriteWidth * 0.7;
     this.height = this.spriteHeight * 0.7;
-    this.x = x;
-    this.y = y;
+    this.x = x - this.width * 0.5;
+    this.y = y - this.height * 0.5;
     this.image = new Image();
     this.image.src = "./files/boom.png";
     this.frame = 0;
     this.timer = 0;
-    this.angle = Math.random() * 6.2;
-    this.sound = new Audio();
-    this.sound.src = "./files/magicSound/Fire impact 1.wav";
   }
   update() {
-    if (this.frame === 0) this.sound.play();
     this.timer++;
     if (this.timer % 8 === 0) {
       this.frame++;
     }
   }
   draw() {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(this.angle);
     // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
     ctx.drawImage(
       this.image,
@@ -46,27 +39,20 @@ class Explosion {
       0,
       this.spriteWidth,
       this.spriteHeight,
-      0 - this.width * 0.5,
-      0 - this.height * 0.5,
+      this.x,
+      this.y,
       this.width,
       this.height
     );
-    ctx.restore();
   }
 }
 
 window.addEventListener("click", function (e) {
-  createAnimation(e);
-});
-// window.addEventListener("mousemove", function (e) {
-//   createAnimation(e);
-// });
-
-function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
   let positionY = e.y - canvasPosition.top;
+
   explosions.push(new Explosion(positionX, positionY));
-}
+});
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
