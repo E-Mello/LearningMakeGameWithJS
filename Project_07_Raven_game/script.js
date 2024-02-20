8a34c25419cc6fc533fa4b686f88888a6080c124
E-Mello <e.mello.dev@gmail.com>
2024-02-20 17:20:58 -0400

    Fix variable name and add try again button

--- Project_07_Raven_game/script.js
+++ Project_07_Raven_game/script.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
   canvas.height = window.innerHeight;
 
   const collisionCanvas = document.getElementById("collisionCanvas");
-  const collisionCtx = canvas.getContext("2d");
+  const collisionCtx = collisionCanvas.getContext("2d");
   collisionCanvas.width = window.innerWidth;
   collisionCanvas.height = window.innerHeight;
 
@@ -34,7 +34,7 @@ window.addEventListener("load", function () {
       this.y = Math.random() * (canvas.height - this.height);
       this.directionX = Math.random() * 5 + 3;
       this.directionY = Math.random() * 5 - 2.5;
-      this.markedForDelete = false;
+      this.markedForDeletion = false;
       this.image = new Image();
       this.image.src = "./raven.png";
       this.frame = 0;
@@ -62,7 +62,7 @@ window.addEventListener("load", function () {
       }
       this.x -= this.directionX;
       this.y += this.directionY;
-      if (this.x < 0 - this.width) this.markForDelete = true;
+      if (this.x < 0 - this.width) this.markedForDeletion = true;
       this.timeSinceFlap += deltatime;
       if (this.timeSinceFlap > this.flapInterval) {
         if (this.frame > this.maxFrame) this.frame = 0;
@@ -100,7 +100,7 @@ window.addEventListener("load", function () {
   class Explosion {
     constructor(x, y, size) {
       this.image = new Image();
-      this.image.src = "boom.png";
+      this.image.src = "./boom.png";
       this.spriteWidth = 200;
       this.spriteHeight = 179;
       this.size = size;
@@ -111,7 +111,7 @@ window.addEventListener("load", function () {
       this.sound.src = "./Fire impact 1.wav";
       this.timeSinceLastFrame = 0;
       this.frameInterval = 200;
-      this.markedForDelete = false;
+      this.markedForDeletion = false;
     }
     update(deltatime) {
       if (this.frame === 0) this.sound.play();
@@ -119,7 +119,7 @@ window.addEventListener("load", function () {
       if (this.timeSinceLastFrame > this.frameInterval) {
         this.frame++;
         this.timeSinceLastFrame = 0;
-        if (this.frame > 5) this.markedForDelete = true;
+        if (this.frame > 5) this.markedForDeletion = true;
       }
     }
     draw() {
@@ -141,18 +141,18 @@ window.addEventListener("load", function () {
   class Particle {
     constructor(x, y, size, color) {
       this.size = size;
-      this.x = x + this.size * 0.5 + Math.random() * 50 - 25;
+      this.x = x + this.size / 2 + Math.random() * 50 - 25;
       this.y = y + this.size / 3 + Math.random() * 50 - 25;
       this.radius = (Math.random() * this.size) / 10;
       this.maxRadius = Math.random() * 20 + 35;
-      this.markForDelete = false;
+      this.markedForDeletion = false;
       this.speedX = Math.random() * 1 + 0.5;
       this.color = color;
     }
     update() {
       this.x += this.speedX;
       this.radius += 0.3;
-      if (this.radius > this.maxRadius - 1) this.markForDelete = true;
+      if (this.radius > this.maxRadius - 1) this.markedForDeletion = true;
     }
     draw() {
       ctx.save();
@@ -186,10 +186,31 @@ window.addEventListener("load", function () {
       canvas.width / 2,
       canvas.height / 2 - 55
     );
+
+    // Draw the "try again" button
+    ctx.fillStyle = "#4CAF50"; // Green button color
+    ctx.fillRect(canvas.width / 4, canvas.height * 0.5, canvas.width / 2, 55);
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Try Again", canvas.width / 2, canvas.height / 2 + 50, 75);
+
+    // Add click event listener to the button
+    canvas.addEventListener("click", function (e) {
+      if (gameOver == true) {
+        // Restart the game
+        gameOver = false;
+        score = 0;
+        ravens = [];
+        particles = [];
+        explosions = [];
+        animate(0);
+      }
+    });
   }
 
   window.addEventListener("click", function (e) {
     const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
+    console.log(detectPixelColor);
     const pc = detectPixelColor.data;
     ravens.forEach((object) => {
       if (
@@ -197,10 +218,11 @@ window.addEventListener("load", function () {
         object.randomColors[1] === pc[1] &&
         object.randomColors[2] === pc[2]
       ) {
-        //collision detected
-        object.markedForDelete = true;
+        // collision detected
+        object.markedForDeletion = true;
         score++;
         explosions.push(new Explosion(object.x, object.y, object.width));
+        console.log(explosions);
       }
     });
   });
@@ -223,9 +245,9 @@ window.addEventListener("load", function () {
       object.update(deltatime)
     );
     [...particles, ...ravens, ...explosions].forEach((object) => object.draw());
-    ravens = ravens.filter((object) => !object.markedForDelete);
-    explosions = explosions.filter((object) => !object.markedForDelete);
-    particles = particles.filter((object) => !object.markForDelete);
+    ravens = ravens.filter((object) => !object.markedForDeletion);
+    explosions = explosions.filter((object) => !object.markedForDeletion);
+    particles = particles.filter((object) => !object.markedForDeletion);
     if (!gameOver) requestAnimationFrame(animate);
     else drawGameOver();
   }
